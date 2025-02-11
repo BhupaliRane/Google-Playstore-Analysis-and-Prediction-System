a806f58660247d7d55c562e4a91ca0a2a0b0c4f6
vedishkabara <vedishkabara25@gmail.com>
2025-02-11 14:38:53 +0530

    updated url

--- UI.py
+++ UI.py
@@ -21,7 +21,7 @@ def get_base64_image(image_path):
     return f"data:image/png;base64,{base64_str}"
 
 # Load background image'
-background_image = get_base64_image(r"C:\Users\ryran\OneDrive\Desktop\ProjectFolder\Project1\bg.jpg")
+background_image = get_base64_image("bg.jpg")
 
 # Adjust container width dynamically
 page = st.session_state.get("page", "Home")
